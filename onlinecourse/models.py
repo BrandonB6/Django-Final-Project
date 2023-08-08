04f7b3e58f30b40ea693f14356df50a490f27c20
BrandonB6 <babbehr6@gmail.com>
2023-08-07 21:23:38 -0400

    lab 3

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -127,12 +127,9 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.choice_text
-
+    choice_text = models.CharField(max_length=30)
+    is_correct = models.BooleanField()
+    
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
