e3667ea8f965b3b74430cf3a2951621ccb5c6b91
BrandonB6 <babbehr6@gmail.com>
2023-08-15 12:38:08 -0400

    final

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -143,24 +143,16 @@ def show_exam_result(request, course_id, submission_id):
     context = {}
     
     course      = get_object_or_404(Course, pk=course_id)
-    submission  = get_object_or_404(Submission, pk=submission_id)
-    
-    selected_choices = submission.choices.all()
-    
-    total_grade, grade = 0, 0
-    
-    for question in course.question_set.all():
-        total_grade += question.grade
-        if question.is_get_score(selected_choices):
-            grade += question.grade
-    
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+    total_score = 0
+    for choice in choices:
+        if choice.is_correct:
+            total_score += choice.question.grade
     context['course'] = course
-    context['selected_choices'] = selected_choices
-    context['total_grade'] = total_grade
-    context['minimum_to_pass'] = total_grade * 0.8
-    context['grade'] = grade
-    
-    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+    context['grade'] = total_score
+    context['choices'] = choices
 
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
